fc76863659bee2b030e8eea7e3300879886e8edf
JosephMoreno <jg.moreno2001@gmail.com>
2021-02-16 02:05:50 +0000

    i think i finished but theres an error

--- assignment1-2.cpp
+++ assignment1-2.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std ;
 
-int(main)
+int main()
+
 {
   int faren ; 
   int celsiuss ; 
 
   cout << "Whats the tempertue in celsius?" ; 
   cin >> celsiuss ;
+
+  faren = celsiuss * 9/5 + 32
+  cout << faren << "is the fahrenheit temperture and the celsius tempertue is" <<celsiuss << endl ;
 
 }
