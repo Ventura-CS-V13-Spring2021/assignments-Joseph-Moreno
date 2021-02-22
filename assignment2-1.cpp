c116e1bef1c76ef5f656d1d4c7fe00763986ee51
JosephMoreno <jg.moreno2001@gmail.com>
2021-02-22 21:12:16 +0000

    starting project

---
+++ assignment2-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int number1, number2, number3;
+     cout << "Enter three integer values : " ;
+     cin >> number1 >> number2 >> number3 ;
+
+      current_min = number1;
+
+      if (current_min > number2)
+     return 0;
+}
