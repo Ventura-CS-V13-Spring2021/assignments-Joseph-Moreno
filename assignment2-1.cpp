2a6971aa32ed3dc7fe13bbd4e569aaab9ce80c60
JosephMoreno <jg.moreno2001@gmail.com>
2021-02-22 21:16:42 +0000

    finished

--- assignment2-1.cpp
+++ assignment2-1.cpp
@@ -3,11 +3,16 @@ using namespace std;
 int main()
 {
      int number1, number2, number3;
+     int current_min ;
      cout << "Enter three integer values : " ;
      cin >> number1 >> number2 >> number3 ;
 
       current_min = number1;
 
       if (current_min > number2)
+        current_min = number2 ;
+     if (current_min > number3)
+        current_min = number3 ;
+      cout << "min is " << current_min << endl;
      return 0;
 }
