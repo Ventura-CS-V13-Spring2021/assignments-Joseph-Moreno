9db8d80c92792e835bd824b0dc732aae9a3e8291
JosephMoreno <jg.moreno2001@gmail.com>
2021-02-22 21:30:06 +0000

    starting project

---
+++ assignment2-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int number1, number2, number3;
+     cout << "Enter three integer values : " ;
+     cin >> number1 >> number2 >> number3 ;
+
+     if (number1 = number2 = number3)
+        cout << "All numbers are the same" << endl ;
+      else if number1 = number2 
+        
+        
+
+     return 0;
+}
