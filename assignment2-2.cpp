3597816ecc884663081e3099dfc4840f337e65ed
JosephMoreno <jg.moreno2001@gmail.com>
2021-02-22 22:54:27 +0000

    added on and testing

--- assignment2-2.cpp
+++ assignment2-2.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main()
 {
-     int number1, number2, number3;
+     int num1, num2, num3;
      cout << "Enter three integer values : " ;
-     cin >> number1 >> number2 >> number3 ;
-
-     if (number1 = number2)
-        
-
+     cin >> num1 >> num2 >> num3 ;
+      if (num1 == num2 && num1 == num3) 
+        cout << "all numbers are the same" << endl ;
+      else if (num1 != num2 && num2 != num3)
+        cout << "all numbers are distinct" << endl ;
+     
      return 0;
 }
