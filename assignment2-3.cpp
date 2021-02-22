4c18632b576928ebc0442675bc493eff907474e1
JosephMoreno <jg.moreno2001@gmail.com>
2021-02-22 23:40:00 +0000

    added lower case

--- assignment2-3.cpp
+++ assignment2-3.cpp
@@ -10,9 +10,15 @@ int main()
      {
        case 'A' : cout << "Your choice is A .\n ";
         break ;
-       case 'B': cout << "Your choice is B .\n ";
+       case 'B' : cout << "Your choice is B .\n ";
         break ; 
         case 'C': cout << "Your choice is C .\n ";
+         break ; 
+      case 'a' : cout << "Your choice is A .\n ";
+        break ;
+       case 'b' : cout << "Your choice is B .\n ";
+        break ; 
+        case 'c': cout << "Your choice is C .\n ";
          break ; 
         default:cout << "You didnt choose A, B, or C. \n" ;
       }
