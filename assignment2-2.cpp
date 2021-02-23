fd359c7fa951db6d948ed5e18ea7c9bbfb4818ff
JosephMoreno <jg.moreno2001@gmail.com>
2021-02-23 18:36:51 +0000

    actually completed. fixed the error where if the 1st int and 3in were the same, it would say all nums are the same

--- assignment2-2.cpp
+++ assignment2-2.cpp
@@ -5,9 +5,9 @@ int main()
      int num1, num2, num3;
      cout << "Enter three integer values : " ;
      cin >> num1 >> num2 >> num3 ;
-      if (num1 == num2 && num1 == num3) 
+      if (num1 == num2 && num1 == num3 && num2 == num3) 
         cout << "all numbers are the same" << endl ;
-      else if (num1 != num2 && num2 != num3)
+      else if (num1 != num2 && num2 != num3 && num1 != num3)
         cout << "all numbers are distinct" << endl ;
       else if (cout << "there are two duplicated numbers" << endl) ;
 	  // Looks good, but when the two numbers are same, 
