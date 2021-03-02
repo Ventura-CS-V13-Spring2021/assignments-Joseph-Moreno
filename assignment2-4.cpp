e9d5bcacc692fb7be21a9f069ff3ab117d9acbf3
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-02 20:31:46 +0000

    starting project

---
+++ assignment2-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  char letter ;
+  letter = 65; 
+
+  while(letter < 70)
+  {cout << letter << "" ;
+  letter ++ ;
+  }
+
+
+  return 0 ; 
+}
