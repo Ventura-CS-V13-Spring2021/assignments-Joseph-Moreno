3f529d740c7098ab9183ca093245fb0589dc5553
Kyuwoong Lee <kwooler@gmail.com>
2021-03-10 04:43:53 +0000

    Kyu

--- assignment2-4.cpp
+++ assignment2-4.cpp
@@ -3,32 +3,40 @@ using namespace std;
 int main()
 {
 
-  char letter ;
-  letter = 65; 
+	char letter ;
+	letter = 65; 
 
-  while(letter < 91)
-  {
+	while(letter < 91)
+	{
+		cout << letter << " " ;
+		letter ++ ;
+		if (letter % 5 == 0){
+			cout << "\n" ;
+		}
+	}
+//   while(letter < 91)
+//   {
   
-  cout << letter << " " ;
-  letter ++ ;
-    if (letter == 70){
+//   cout << letter << " " ;
+//   letter ++ ;
+//     if (letter == 70){
 
-     cout << "\n" ;}
+//      cout << "\n" ;}
      
-      else if (letter == 75){
-      cout << "\n" ;
-      }
-      else if (letter == 80){
-      cout << "\n" ;
-      }
-      else if (letter == 85){
-      cout << "\n" ;
-      }
-      else if (letter == 90){
-      cout << "\n" ;
-      }
+//       else if (letter == 75){
+//       cout << "\n" ;
+//       }
+//       else if (letter == 80){
+//       cout << "\n" ;
+//       }
+//       else if (letter == 85){
+//       cout << "\n" ;
+//       }
+//       else if (letter == 90){
+//       cout << "\n" ;
+//       }
      
-  }
+//   }
 
   return 0 ; 
 }
