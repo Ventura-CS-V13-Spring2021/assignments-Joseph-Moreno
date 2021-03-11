95234712a87a6dc5af57df7bc17e03eec7ee4094
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-11 06:06:05 +0000

    starting project

---
+++ assignment2-5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int range1 , range2 ;
+
+cout << "List 2 integers to find the prime numbers in the range" 
+cin >> range1 >> range2;
+
+cout << range1 << range2 << endl;
+
+
+
+
+
+}
