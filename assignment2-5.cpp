ca3a8a24efa467e268112ef39ec64a90c3e33e93
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-13 06:44:37 +0000

    finished the project

--- assignment2-5.cpp
+++ assignment2-5.cpp
@@ -3,15 +3,22 @@ using namespace std;
 int main()
 {
 
+int num,i ;
 int range1 , range2 ;
 
-cout << "List 2 integers to find the prime numbers in the range" 
+cout << "List 2 integers to find the prime numbers in the range: " ;
 cin >> range1 >> range2;
 
-cout << range1 << range2 << endl;
-
-
-
-
+for(int num=range1 ; num <= range2 ; num ++)
+{
+    for (i=2 ; i < num ; i++ )
+    {
+      if(num % i == 0)
+        break;
+      
+    }
+    if (i== num)
+      cout<< num  << "\n"  ;
+}
 
 }
