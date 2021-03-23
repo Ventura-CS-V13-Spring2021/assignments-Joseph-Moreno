1b764bf804dcfe717cb4c2f9f3a7b57148e52026
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-23 04:56:29 +0000

    added the code to find prime numbers. need to make sure that range 2 is bigger than range 1

--- quiz2.cpp
+++ quiz2.cpp
@@ -14,6 +14,17 @@ int range1 , range2 ;
 cout << "List 2 integers to find the prime numbers in the range: " ;
 cin >> range1 >> range2;
 
-
+for(int num=range1 ; num <= range2 ; num ++)
+{
+    for (i=2 ; i < num ; i++ )
+    {
+      if(num % i == 0)
+        break;
+      
+    }
+    if (i== num)
+      cout<< num  << "\n"  ;
+  }
+}
 
 
