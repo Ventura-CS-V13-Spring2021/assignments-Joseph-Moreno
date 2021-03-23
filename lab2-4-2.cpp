cd9104bba6dcae9972a5b3614a9a0767bcf5bb8d
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-23 02:46:49 +0000

    starting quiz

--- lab2-4-2.cpp
+++ lab2-4-2.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
 int main()
 {
   int rdnum;
   int n ;
   int sum = 0;
-  ofstream  rdfile;
+  ifstream  rdfile;
 
   rdfile.open("rdnum.txt");
 
-  while (rdfile.fail())
+  while (! rdfile.fail())
   {
 
-    rdfile >> rdnum;
-    sum += rdnum ;
-    cout << rdnum << endl ;
+      rdfile >> rdnum;
+      sum += rdnum ;
+     cout << rdnum << endl ;
   }
-  cout << "Sum" << sum << endl; 
+  cout << "Sum " << sum << endl; 
 
   rdfile.close ();
 }
---
+++ quiz2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+  
+
+  i = (rand()(0-100)-0;
+  cout << i << endl;
+}
