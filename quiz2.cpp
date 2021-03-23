e617fc91b9c953fa41051d4a94ef51635d32e9c9
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-23 04:19:23 +0000

    i restarted and it started working. i looked at an old assignment

--- quiz2.cpp
+++ quiz2.cpp
@@ -9,13 +9,14 @@ using namespace std;
 int main()
 {
 
-int num1 , num2 , num3 ;
-cout << "enter 3 integer values between 0 and 99 : " ;
-cin >> num1 , num2 , num3 ;
-
-if (num1 == num2 && num1 == num3 && num2 == num3)
-  cout << "all the same" << endl ;
-
+     int num1, num2, num3;
+     cout << "Enter three integer values in range from 0 to 99: " ;
+     cin >> num1 >> num2 >> num3 ;
+      if (num1 == num2 && num1 == num3 && num2 == num3) 
+        cout << "all numbers are the same " << num1 << num2 << num3 << endl ;
+      else if (num1 != num2 && num2 != num3 && num1 != num3)
+        cout << "all numbers are distinct" << endl ;
+      else if (cout << "there are two duplicated numbers" << endl) ;
 
 }
 
