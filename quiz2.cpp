eae1ec28e495691848795d55d011460830bd42c2
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-23 02:53:49 +0000

    updated

--- quiz2.cpp
+++ quiz2.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
 
 int main()
 {
-  
 
-  i = (rand()(0-100)-0;
-  cout << i << endl;
+
+int quizrdm ;
+int n;
+n=4 ;
+
+for(int i; i <n ; i++)
+{
+   quizrdm =random () %100 ;
+   cout << quizrdm << endl ;
 }
+
+
+}
+
