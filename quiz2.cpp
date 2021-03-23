d10f379fe169299b46402264e1928c0aee74aea4
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-23 05:02:28 +0000

    completed

--- quiz2.cpp
+++ quiz2.cpp
@@ -14,8 +14,12 @@ int range1 , range2 ;
 cout << "List 2 integers to find the prime numbers in the range: " ;
 cin >> range1 >> range2;
 
-for(int num=range1 ; num <= range2 ; num ++)
-{
+if (range1 > range2){
+  cout << "cannot do since range 1 is bigger than range 2" << endl ;
+}
+else if(range1 < range2)
+  for(int num=range1 ; num <= range2 ; num ++)
+  {
     for (i=2 ; i < num ; i++ )
     {
       if(num % i == 0)
