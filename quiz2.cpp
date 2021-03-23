1bf923a136b0fb9b709738f8326742328eb4dc45
JosephMoreno <jg.moreno2001@gmail.com>
2021-03-23 04:26:57 +0000

    completed

--- quiz2.cpp
+++ quiz2.cpp
@@ -13,10 +13,14 @@ int main()
      cout << "Enter three integer values in range from 0 to 99: " ;
      cin >> num1 >> num2 >> num3 ;
       if (num1 == num2 && num1 == num3 && num2 == num3) 
-        cout << "all numbers are the same " << num1 << num2 << num3 << endl ;
+        cout << "all numbers are the same: " << num1  << num2  << num3 << endl ;
       else if (num1 != num2 && num2 != num3 && num1 != num3)
         cout << "all numbers are distinct" << endl ;
-      else if (cout << "there are two duplicated numbers" << endl) ;
+      else if (num1 == num2 && num2 != num3 && num1 != num3)
+        cout << "there are two duplicated numbers" << num1 << num2 << endl ;
+      else if (num1 != num2 && num2 == num3 && num1 != num3)
+        cout << "there are two duplicated numbers" << num3 << num2 << endl ;
+      else if (cout << "there are two duplicated numbers  :  " << num1 << num3 << endl );
 
 }
 
