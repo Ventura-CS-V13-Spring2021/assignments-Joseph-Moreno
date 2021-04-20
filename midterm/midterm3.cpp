4155fa38f28ece6af143cd555c55e77121f3bd9b
JosephMoreno <>
2021-04-20 03:38:48 +0000

    random number generation and max

--- midterm/midterm3.cpp
+++ midterm/midterm3.cpp
@@ -6,5 +6,22 @@ using namespace std ;
 
 int main()
 {
+  int rdm1;
+  int rdm2;
+  int rdm3;
+
+  rdm1 = rand () %10 ;
+  rdm2 = rand () %10 ;
+  rdm3 = rand () %10 ;
+
+  int min ;
+  int max;
+  if (rdm1 > rdm2 && rdm1 > rdm3 )
+    rdm1 = max;
+    else (rdm2 > rdm1 && rdm2 > rdm3);
+     rdm2 = max;
+    else (rdm3 = max);
+
+
 
 }
